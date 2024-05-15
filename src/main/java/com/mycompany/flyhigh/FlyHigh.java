3ed8a72980a426e65021e513f06c0ec408371c57
Soumyadeep20 <soumyadeeppodder20@gmail.com>
2024-05-15 23:49:32 +0530

    V1

---
+++ src/main/java/com/mycompany/flyhigh/FlyHigh.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.flyhigh;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class FlyHigh {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
